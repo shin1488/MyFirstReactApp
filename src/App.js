2fa647a81baecbe75b72533db7f373dffa60090b
shin1488 <syc1488@naver.com>
2020-07-28 00:00:14 +0900

    ~128

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
 import React from 'react';
 
-const catKind = [
-  {
-    id : 'oC',
-    name : 'Ollie',
-    image : 'https://www.dailydot.com/wp-content/uploads/2018/10/olli-the-polite-cat-800x400.jpg',
-  },
-  {
-    id : 'bC',
-    name : 'Butter Cat',
-    image : 'https://preview.redd.it/jbpcreug9sb11.jpg?width=960&crop=smart&auto=webp&s=744a257c3fb19d1e92640a5053fe6adb339f6943',
-  },
-];
+class App extends React.Component {
+  state = {
+    count : 0,
+  };
 
-function Cat({ kind, picture }) {
-  return (
-    <div>
-      <h2>I love Cats, {kind}</h2>
-      <img src = {picture} alt = {kind} />
-    </div>
-  );
-}
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 999,
+    }));
+  };
 
-function App() {
-  return (
-    <div>
-      {catKind.map(ki => (
-      <Cat key = {ki.id} kind = {ki.name} picture = {ki.image} />
-      ))}
-    </div>
-  )
+  minus = () => {
+    this.setState(current => ({
+      count: current.count - 1,
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+       <button onClick={this.add}>Add</button>
+       <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
 }
 
 export default App;
